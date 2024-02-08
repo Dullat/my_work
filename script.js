57581d650d117c78c9ab9f60ea4d7f38edd411b4
DuLLatJatt <142017979+Dullat@users.noreply.github.com>
2024-02-08 13:06:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector('.toggle-btn');
 const body = document.querySelector('body');
 const menuBtn = document.querySelector('.menu-btn');
 const dashboard = document.querySelector('.dashboard');
+const overlay = document.querySelector('i');
 
 toggle.addEventListener('click', () => {
     toggle.toggleAttribute('dark');
@@ -10,4 +11,14 @@ toggle.addEventListener('click', () => {
 
 menuBtn.addEventListener('click', () => {
     dashboard.setAttribute('visible','');
+    overlay.setAttribute('visible','')
+    document.body.style.overflow = 'hidden';
+})
+
+document.addEventListener('click', (e) => {
+    if((e.target !== dashboard) && (e.target !== menuBtn)){
+        dashboard.removeAttribute('visible');
+        overlay.removeAttribute('visible');
+        document.body.style.overflow = '';
+    }
 })
